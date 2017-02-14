e121fca2ccc635c3990c18dadf3eacd53c10f609
Manjunath Chandrashekar <manjunath.c1@myntra.com>
2017-02-14 19:01:22 +0530

    Initial Commit

---
+++ sample/src/test/java/com/github/android/sample/SearchDialogUnitTest.java
@@ -0,0 +1,12 @@
+package com.github.android.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchDialogUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
